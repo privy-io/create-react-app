69293b67423d19078f92ec471b8587a62d433e23
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2023-02-01 12:02:18 -0800

    build: update to deployable demo, uptick version (#1)
    * Switch to env var app id
    
    * Remove unused boilerplate
    
    * Update to latest test build

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { PrivyProvider } from '@privy-io/react-auth';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PrivyProvider 
-      appId='your-privy-app-id'
+    <PrivyProvider
+      appId={process.env.REACT_APP_PRIVY_APP_ID}
       onSuccess={(user) => console.log(`User ${user.id} logged in!`)}
     >
       <App />
