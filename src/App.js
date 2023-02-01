f2ebdc62433f3515310ac6061b4bd3afc05e3f57
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2023-02-01 14:56:03 -0500

    Remove unused boilerplate

--- src/App.js
+++ src/App.js
@@ -14,34 +14,20 @@ function App() {
       <header className="App-header">
         {/* If the user is not authenticated, show a login button */}
         {/* If the user is authenticated, show the user object and a logout button */}
-        {authenticated ?
+        {authenticated ? (
           <div>
             <textarea
               value={JSON.stringify(user, null, 2)}
-              style={{width: '600px', height: '250px'}}
+              style={{ width: "600px", height: "250px" }}
             />
             <br />
-            <button 
-              onClick={logout}
-              style={{marginTop: '20px'}}
-            >
+            <button onClick={logout} style={{ marginTop: "20px" }}>
               Log Out
             </button>
           </div>
-           :
+        ) : (
           <button onClick={login}>Log In</button>
-        }
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        )}
       </header>
     </div>
   );
