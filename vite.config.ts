28196f70da94840457bad521626cbc40c474dd00
siddesh-privy <siddesh@privy.io>
2025-09-16 19:23:05 +0700

    use build folder for build

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), nodePolyfills()],
+  build: {
+    outDir: "build",
+  },
 });
